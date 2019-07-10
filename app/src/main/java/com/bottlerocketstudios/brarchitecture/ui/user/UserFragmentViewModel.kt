4f556b4a9e572047020fbb9ed80e48aeb102ed2c
Chris K <chris.koeberle@bottlerocketstudios.com>
2019-07-10 09:54:49 -0500

    Display user info

--- app/src/main/java/com/bottlerocketstudios/brarchitecture/ui/user/UserFragmentViewModel.kt
+++ app/src/main/java/com/bottlerocketstudios/brarchitecture/ui/user/UserFragmentViewModel.kt
@@ -1,9 +1,10 @@
 package com.bottlerocketstudios.brarchitecture.ui.user
 
 import android.app.Application
-import com.bottlerocketstudios.brarchitecture.ui.ScopedViewModel
+import com.bottlerocketstudios.brarchitecture.infrastructure.repository.BitbucketRepository
+import com.bottlerocketstudios.brarchitecture.ui.RepoViewModel
 
-class UserFragmentViewModel(app: Application) : ScopedViewModel(app) {
+class UserFragmentViewModel(app: Application, repo: BitbucketRepository) : RepoViewModel(app, repo) {
     override fun onCleared() {
         super.onCleared()
         doClear()
