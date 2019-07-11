7e08d8b7af80bc00679571d83a3668368f214766
Chris K <chris.koeberle@bottlerocketstudios.com>
2019-07-11 13:53:59 -0500

    Make the fragment tests more functional

--- app/src/androidTest/java/com/bottlerocketstudios/brarchitecture/ui/repository/RepositoryFragmentUiTest.kt
+++ app/src/androidTest/java/com/bottlerocketstudios/brarchitecture/ui/repository/RepositoryFragmentUiTest.kt
@@ -1,10 +1,15 @@
 package com.bottlerocketstudios.brarchitecture.ui.repository
 
-// None of this is going to work
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.TypeTextAction
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.bottlerocketstudios.brarchitecture.R
 import com.bottlerocketstudios.brarchitecture.ui.auth.LoginFragment
+import org.hamcrest.Matchers.not
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -14,5 +19,9 @@ class RepositoryFragmentUiTest {
     @Test
     fun launchAFragment() {
         val scenario = launchFragmentInContainer<LoginFragment>(themeResId = R.style.AppTheme)
+        onView(withId(R.id.login)).check(matches(not(isEnabled())))
+        onView(withId(R.id.id)).perform(TypeTextAction("test@example.com"))
+        onView(withId(R.id.password)).perform(TypeTextAction("password1"))
+        onView(withId(R.id.login)).check(matches(isEnabled()))
     }
 }
--- app/src/androidTest/java/com/bottlerocketstudios/brarchitecture/ui/user/UserFragmentUiTest.kt
+++ app/src/androidTest/java/com/bottlerocketstudios/brarchitecture/ui/user/UserFragmentUiTest.kt
@@ -1,8 +1,16 @@
 package com.bottlerocketstudios.brarchitecture.ui.user
 
-// None of this is going to work
+import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.bottlerocketstudios.brarchitecture.R
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class UserFragmentUiTest
+class UserFragmentUiTest {
+
+    @Test
+    fun launchAFragment() {
+        val scenario = launchFragmentInContainer<UserFragment>(themeResId = R.style.AppTheme)
+    }
+}
