c640e0fac3df25f70604ba9f522d1ddaebc2ae0f
Ivan Telisman <ivan.telisman@bottlerocketstudios.com>
2021-10-24 16:05:37 -0500

    code cleanup in MainActivityVM

--- app/src/main/java/com/bottlerocketstudios/brarchitecture/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/bottlerocketstudios/brarchitecture/ui/MainActivityViewModel.kt
@@ -1,30 +1,25 @@
 package com.bottlerocketstudios.brarchitecture.ui
 
 import android.app.Application
-import androidx.lifecycle.viewModelScope
 import com.bottlerocketstudios.brarchitecture.data.buildconfig.BuildConfigProvider
 import com.bottlerocketstudios.brarchitecture.data.model.Repository
 import com.bottlerocketstudios.brarchitecture.data.repository.BitbucketRepository
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
 
 class MainActivityViewModel(app: Application, val repo: BitbucketRepository, buildConfigProvider: BuildConfigProvider) : BaseViewModel(app) {
     val selectedRepo: StateFlow<Repository> = MutableStateFlow(Repository())
     val showToolbar: StateFlow<Boolean> = MutableStateFlow(false)
     val title: StateFlow<String> = MutableStateFlow("")
 
-    val avatarUrl = repo.user.map { it?.avatarUrl.orEmpty() }.stateIn(viewModelScope, SharingStarted.Lazily, "")
-    val displayName = repo.user.map { it?.displayName.orEmpty() }.stateIn(viewModelScope, SharingStarted.Lazily, "")
-    val username = repo.user.map { it?.username.orEmpty() }.stateIn(viewModelScope, SharingStarted.Lazily, "")
+    val avatarUrl = repo.user.map { it?.avatarUrl.orEmpty() }.groundState("")
+    val displayName = repo.user.map { it?.displayName.orEmpty() }.groundState("")
+    val username = repo.user.map { it?.username.orEmpty() }.groundState("")
 
     val devOptionsEnabled = buildConfigProvider.isDebugOrInternalBuild
 
     fun selectRepo(repo: Repository) = selectedRepo.set(repo)
-
     fun showToolbar(show: Boolean) = showToolbar.setNullable(show)
-
     fun setTitle(title: String) = this.title.set(title)
 }
