f82cbe6e37b6257ae47c6ca37f96d133c3c661d6
Jared Stockton <jared.stockton@bottlerocketstudios.com>
2022-02-09 16:20:09 -0600

    Disable Koin definition overrides in app code and modify test code to remove deprecation warnings
    Additionally, use `BaseTest.inlineKoinSingle{}` to showcase an example usage. The Koin test graph's `Clock` is overridden by the test function provided `Clock`.

--- app/src/test/java/com/bottlerocketstudios/brarchitecture/ui/repository/RepositoryViewModelTest.kt
+++ app/src/test/java/com/bottlerocketstudios/brarchitecture/ui/repository/RepositoryViewModelTest.kt
@@ -1,13 +1,19 @@
 package com.bottlerocketstudios.brarchitecture.ui.repository
 
+import com.bottlerocketstudios.brarchitecture.R
+import com.bottlerocketstudios.brarchitecture.data.model.Repository
 import com.bottlerocketstudios.brarchitecture.test.BaseTest
 import com.bottlerocketstudios.brarchitecture.test.KoinTestRule
 import com.bottlerocketstudios.brarchitecture.test.TestModule
 import com.bottlerocketstudios.brarchitecture.ui.ViewModelItem
+import com.bottlerocketstudios.brarchitecture.ui.util.StringIdHelper
 import com.google.common.truth.Truth.assertThat
 import org.junit.Rule
-import org.mockito.kotlin.mock
 import org.junit.Test
+import java.time.Clock
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 class RepositoryViewModelTest : BaseTest() {
 
@@ -15,10 +21,12 @@ class RepositoryViewModelTest : BaseTest() {
     val koinRule = KoinTestRule(TestModule.generateMockedTestModule())
 
     @Test
-    fun repositoryViewModel_shouldHaveMembers_whee() {
-        val rvm = RepositoryViewModel(mock {})
+    fun repositoryViewModel_initialized_configuredAsExpected() {
+        inlineKoinSingle { Clock.fixed(Instant.parse("2022-01-26T13:00:00.00Z"), ZoneId.of("UTC")) } // example of using inlineKoinSingle to override Clock in koin module graph
+        val rvm = RepositoryViewModel(Repository(updated = ZonedDateTime.now(Clock.fixed(Instant.parse("2022-01-26T08:00:00.00Z"), ZoneId.of("UTC")))))
 
         assertThat(rvm.repository).isNotNull()
         assertThat(rvm.getItem(0)).isInstanceOf(ViewModelItem::class.java)
+        assertThat(rvm.formattedUpdateTime).isEqualTo(StringIdHelper.Id(R.string.today))
     }
 }
