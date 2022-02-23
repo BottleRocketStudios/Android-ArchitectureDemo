f701ed1809bc1d3ab3dd5a78dc51d01627525dce
Ian Slinker <ian.slinker@bottlerocketstudios.com>
2022-02-23 16:48:06 -0600

    Created state classes and compose screen stubs for all fragments

---
+++ compose/src/main/java/com/bottlerocketstudios/compose/snippets/SnippetsBrowser.kt
@@ -0,0 +1,14 @@
+package com.bottlerocketstudios.compose.snippets
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+
+data class SnippetsBrowserScreenState(
+    val snippets: State<List<SnippetUiModel>>,
+    val onCreateSnippetClicked: () -> Unit
+)
+
+@Composable
+fun SnippetsBrowserScreen(state: SnippetsBrowserScreenState) {
+
+}
