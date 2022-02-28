39096f9640ed123322d6cdd91efdc1041958e2d8
Ian Slinker <ian.slinker@bottlerocketstudios.com>
2022-02-28 14:35:17 -0600

    Splash screen converted to compose

--- app/src/main/java/com/bottlerocketstudios/brarchitecture/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/bottlerocketstudios/brarchitecture/ui/splash/SplashFragment.kt
@@ -1,12 +1,41 @@
 package com.bottlerocketstudios.brarchitecture.ui.splash
 
-import com.bottlerocketstudios.brarchitecture.R
-import com.bottlerocketstudios.brarchitecture.databinding.SplashFragmentBinding
-import com.bottlerocketstudios.brarchitecture.ui.BaseDataBindingFragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.bottlerocketstudios.brarchitecture.ui.BaseFragment
+import com.bottlerocketstudios.compose.resources.ArchitectureDemoTheme
+import com.bottlerocketstudios.compose.splash.SplashScreen
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SplashFragment : BaseDataBindingFragment<SplashFragmentViewModel, SplashFragmentBinding>() {
+class SplashFragment : BaseFragment<SplashFragmentViewModel>() {
     override val fragmentViewModel: SplashFragmentViewModel by viewModel()
 
-    override fun getLayoutRes(): Int = R.layout.splash_fragment
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            // Dispose the Composition when viewLifecycleOwner is destroyed
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
+            )
+
+            setContent {
+                OuterScreenContent()
+            }
+        }
+    }
+
+    @Composable
+    private fun OuterScreenContent() {
+        ArchitectureDemoTheme {
+            SplashScreen()
+        }
+    }
 }
