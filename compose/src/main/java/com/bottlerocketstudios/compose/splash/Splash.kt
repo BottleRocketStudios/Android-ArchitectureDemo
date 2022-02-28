96185a4c2ef0508acc6ece5e2ad5ad616bd5797f
ianslinker-br <89795052+ianslinker-br@users.noreply.github.com>
2022-02-28 16:14:49 -0600

    Splash screen converted to compose (#44)

--- compose/src/main/java/com/bottlerocketstudios/compose/splash/Splash.kt
+++ compose/src/main/java/com/bottlerocketstudios/compose/splash/Splash.kt
@@ -1,7 +1,39 @@
 package com.bottlerocketstudios.compose.splash
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.bottlerocketstudios.compose.R
+import com.bottlerocketstudios.compose.resources.ArchitectureDemoTheme
 
 @Composable
 fun SplashScreen() {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = stringResource(id = R.string.splash_image_content_description)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewOuterScreenContent() {
+    ArchitectureDemoTheme {
+        SplashScreen()
+    }
 }
