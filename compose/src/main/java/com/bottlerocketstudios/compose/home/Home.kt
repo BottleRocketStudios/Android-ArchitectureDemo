f47f60608bc1bb54d63fef3567bddae8d38ed57d
Vanya Huaman <vanya.huaman@bottlerocketstudios.com>
2022-03-18 11:24:09 -0500

    Add conditional empty Home state

--- compose/src/main/java/com/bottlerocketstudios/compose/home/Home.kt
+++ compose/src/main/java/com/bottlerocketstudios/compose/home/Home.kt
@@ -1,15 +1,21 @@
 package com.bottlerocketstudios.compose.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.bottlerocketstudios.compose.R
 import com.bottlerocketstudios.compose.resources.Dimens
+import com.bottlerocketstudios.compose.resources.bold
 import com.bottlerocketstudios.compose.util.Preview
 import com.bottlerocketstudios.compose.util.asState
 
@@ -20,19 +26,36 @@ data class HomeScreenState(
 @Composable
 fun HomeScreen(state: HomeScreenState, selectItem: (userRepositoryUiModel: UserRepositoryUiModel) -> Unit) {
     Surface {
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(Dimens.grid_1_5),
-            modifier = Modifier
-                .padding(
-                    start = Dimens.grid_1_5,
-                    end = Dimens.grid_1_5
-                )
-                .fillMaxSize()
-        ) {
-            state.repositories.value.forEach {
-                item {
-                    CardLayout(it, selectItem)
+        Column {
+            Text(
+                text = stringResource(id = R.string.home_repositories),
+                style = MaterialTheme.typography.h1.bold(),
+                modifier = Modifier
+                    .padding(
+                        start = Dimens.grid_2_5,
+                        end = Dimens.grid_1_5,
+                        top = Dimens.grid_3_5,
+                        bottom = Dimens.grid_1_5
+                    )
+            )
+            if (state.repositories.value.isNotEmpty()) {
+                LazyColumn(
+                    verticalArrangement = Arrangement.spacedBy(Dimens.grid_1_5),
+                    modifier = Modifier
+                        .padding(
+                            start = Dimens.grid_1_5,
+                            end = Dimens.grid_1_5
+                        )
+                        .fillMaxSize()
+                ) {
+                    state.repositories.value.forEach {
+                        item {
+                            CardLayout(it, selectItem)
+                        }
+                    }
                 }
+            } else {
+                HomeEmptyLayout()
             }
         }
     }
