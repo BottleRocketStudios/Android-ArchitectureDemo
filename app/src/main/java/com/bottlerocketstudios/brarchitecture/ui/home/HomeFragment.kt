ba4f279a097bc362b646b17f404a8d3fce7aef9d
Vanya Huaman <vanya.huaman@bottlerocketstudios.com>
2022-03-25 11:41:08 -0500

    Pr Update - Remove clock inject from home frag

--- app/src/main/java/com/bottlerocketstudios/brarchitecture/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bottlerocketstudios/brarchitecture/ui/home/HomeFragment.kt
@@ -8,18 +8,14 @@ import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.bottlerocketstudios.brarchitecture.ui.BaseFragment
 import com.bottlerocketstudios.brarchitecture.ui.MainActivityViewModel
 import com.bottlerocketstudios.brarchitecture.ui.repository.RepositoryBrowserData
-import com.bottlerocketstudios.compose.util.formattedUpdateTime
 import com.bottlerocketstudios.compose.home.HomeScreen
 import com.bottlerocketstudios.compose.home.UserRepositoryUiModel
-import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.time.Clock
 
 class HomeFragment : BaseFragment<HomeViewModel>() {
     override val fragmentViewModel: HomeViewModel by viewModel()
     private val activityViewModel: MainActivityViewModel by sharedViewModel()
-    private val clock by inject<Clock>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
         ComposeScreen {
