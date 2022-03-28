36a0f9d90418516cb13db7e6161e1d26cbe43c2c
Colin Shelton <colin.shelton@bottlerocketstudios.com>
2022-03-28 15:05:56 -0500

    PR Cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 rootProject.name = "ArchitectureDemo" // TODO: TEMPLATE - Replace with appropriate project name
 
-// includeBuild("launchpad-compose")
 include(":data")
 include(":app")
 include(":compose")
