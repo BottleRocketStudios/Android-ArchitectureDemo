5173fb42d74d98b17e31606a4f07ebc0ca4d3341
LuisRocketRangel <luis.rangel@bottlerocketstudios.com>
2022-12-09 15:17:26 -0600

    ASAA-39-Jared Feedback Part I
    Requirements:
    -Change JSON Asset file to JSON String
    
    Solution:
    -Did above

--- data/src/main/java/com/bottlerocketstudios/brarchitecture/data/repository/FeatureToggleRepositoryImpl.kt
+++ data/src/main/java/com/bottlerocketstudios/brarchitecture/data/repository/FeatureToggleRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package com.bottlerocketstudios.brarchitecture.data.repository
 
-import android.app.Application
 import com.bottlerocketstudios.brarchitecture.data.converter.toFeatureToggle
 import com.bottlerocketstudios.brarchitecture.data.model.FeatureToggleDto
 import com.bottlerocketstudios.brarchitecture.domain.models.FeatureToggle
@@ -11,14 +10,12 @@ import com.squareup.moshi.Types
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
+import org.intellij.lang.annotations.Language
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 
 @Suppress("TooManyFunctions")
 class FeatureToggleRepositoryImpl(private val moshi: Moshi) : FeatureToggleRepository, KoinComponent {
     // DI
-    private val app: Application by inject()
-
     private val _featureToggles = MutableStateFlow<List<FeatureToggleDto>>(emptyList())
     override val featureToggles: Flow<List<FeatureToggle>> = _featureToggles.map { list -> list.map { it.toFeatureToggle() } }
 
@@ -29,11 +26,29 @@ class FeatureToggleRepositoryImpl(private val moshi: Moshi) : FeatureToggleRepos
     override fun getFeatureTogglesFromJson() {
         val listType = Types.newParameterizedType(List::class.java, FeatureToggleDto::class.java)
         val adapter: JsonAdapter<List<FeatureToggleDto>> = moshi.adapter(listType)
-        val fileJson: String = app.assets.open("featureToggles.json").bufferedReader().use { it.readText() }
-        _featureToggles.value = adapter.fromJson(fileJson) ?: emptyList()
+        _featureToggles.value = adapter.fromJson(FEATURE_TOGGLE_JSON) ?: emptyList()
     }
 
     override fun getFeatureToggle(name: String): Boolean {
         return _featureToggles.value.find { it.name == name }?.value == true
     }
 }
+
+@Language("JSON")
+private const val FEATURE_TOGGLE_JSON =
+    """
+        [
+            {
+              "name": "SHOW_SNIPPETS",
+              "value": false,
+              "defaultValue": true,
+              "requireRestart": false
+            },
+            {
+              "name": "SHOW_PULL_REQUESTS",
+              "value": false,
+              "defaultValue": true,
+              "requireRestart": false
+            }
+        ]
+    """
