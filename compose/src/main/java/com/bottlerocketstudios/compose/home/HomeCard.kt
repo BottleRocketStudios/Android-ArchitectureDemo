9b0d38ffe99791a3818201a2a1a25011e05a63fb
Harish Patel <harishpateldfw@gmail.com>
2026-01-05 13:40:04 -0600

    fix pull requests UI

--- compose/src/main/java/com/bottlerocketstudios/compose/home/HomeCard.kt
+++ compose/src/main/java/com/bottlerocketstudios/compose/home/HomeCard.kt
@@ -2,7 +2,6 @@ package com.bottlerocketstudios.compose.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -28,6 +27,7 @@ import com.bottlerocketstudios.compose.R
 import com.bottlerocketstudios.compose.resources.Colors
 import com.bottlerocketstudios.compose.resources.Dimens
 import com.bottlerocketstudios.compose.resources.brown_grey
+import com.bottlerocketstudios.compose.resources.greyish_brown
 import com.bottlerocketstudios.compose.resources.typography
 import com.bottlerocketstudios.compose.util.Preview
 import com.bottlerocketstudios.compose.util.PreviewComposable
@@ -38,140 +38,200 @@ fun RepositoryCardLayout(
         userRepositoryUiModel: UserRepositoryUiModel,
         selectItem: (userRepositoryUiModel: UserRepositoryUiModel) -> Unit
 ) {
-    Card(
-            elevation = Dimens.plane_3,
-            modifier = Modifier.clickable { selectItem(userRepositoryUiModel) }.wrapContentHeight()
-    ) {
-        Row(
+        Card(
+                elevation = Dimens.plane_3,
                 modifier =
-                        Modifier.padding(
-                                        start = Dimens.grid_1,
-                                        end = Dimens.grid_1,
-                                        top = Dimens.grid_1,
-                                        bottom = Dimens.grid_1
-                                )
-                                .wrapContentHeight(align = Alignment.CenterVertically)
-                                .fillMaxWidth()
+                        Modifier.clickable { selectItem(userRepositoryUiModel) }.wrapContentHeight()
         ) {
-            Box(modifier = Modifier.wrapContentWidth()) {
-                Icon(
-                        painter = painterResource(id = R.drawable.ic_repository),
-                        contentDescription = stringResource(id = R.string.home_repository_icon),
-                        tint = Colors.tertiary,
-                        modifier = Modifier.padding(all = Dimens.grid_2)
-                )
-            }
-            Row(modifier = Modifier.wrapContentHeight(align = Alignment.Top)) {
-                RepoDescriptionComponent(
-                        userRepositoryUiModel = userRepositoryUiModel,
-                        Modifier.weight(2f)
-                )
+                Row(
+                        modifier =
+                                Modifier.padding(
+                                                start = Dimens.grid_1,
+                                                end = Dimens.grid_1,
+                                                top = Dimens.grid_1,
+                                                bottom = Dimens.grid_1
+                                        )
+                                        .wrapContentHeight(align = Alignment.CenterVertically)
+                                        .fillMaxWidth()
+                ) {
+                        Box(modifier = Modifier.wrapContentWidth()) {
+                                Icon(
+                                        painter = painterResource(id = R.drawable.ic_repository),
+                                        contentDescription =
+                                                stringResource(id = R.string.home_repository_icon),
+                                        tint = Colors.tertiary,
+                                        modifier = Modifier.padding(all = Dimens.grid_2)
+                                )
+                        }
+                        Row(modifier = Modifier.wrapContentHeight(align = Alignment.Top)) {
+                                RepoDescriptionComponent(
+                                        userRepositoryUiModel = userRepositoryUiModel,
+                                        Modifier.weight(2f)
+                                )
 
-                Column(modifier = Modifier.weight(1f).padding(top = Dimens.grid_2_5)) {
-                    Text(
-                            userRepositoryUiModel.formattedLastUpdatedTime.getString(),
-                            style = MaterialTheme.typography.h6.copy(fontWeight = FontWeight.Light),
-                            modifier = Modifier.wrapContentHeight().fillMaxWidth()
-                    )
+                                Column(
+                                        modifier =
+                                                Modifier.weight(1f).padding(top = Dimens.grid_2_5)
+                                ) {
+                                        Text(
+                                                userRepositoryUiModel.formattedLastUpdatedTime
+                                                        .getString(),
+                                                style =
+                                                        MaterialTheme.typography.h6.copy(
+                                                                fontWeight = FontWeight.Light
+                                                        ),
+                                                modifier =
+                                                        Modifier.wrapContentHeight().fillMaxWidth()
+                                        )
+                                }
+                        }
                 }
-            }
         }
-    }
 }
 
 @Composable
 fun PullRequestCardLayout(userPullRequestUIModel: UserPullRequestUIModel) {
-    Card(elevation = Dimens.plane_3, modifier = Modifier.fillMaxWidth()) {
-        Row(modifier = Modifier.padding(top = Dimens.grid_2, bottom = Dimens.grid_2)) {
-            @Suppress("MagicNumber")
-            Column(modifier = Modifier.padding(start = Dimens.grid_2_5, end = Dimens.grid_2_5)) {
-                Row {
-                    ResponsiveText(
-                            text = userPullRequestUIModel.pullRequest.title,
-                            textStyle = typography.h3,
-                            modifier = Modifier.padding(end = Dimens.grid_1).weight(3f)
-                    )
-                    Text(
-                            text = userPullRequestUIModel.formattedLastUpdatedTime.getString(),
-                            fontSize = 10.sp
-                    )
+        Card(elevation = Dimens.plane_3, modifier = Modifier.fillMaxWidth()) {
+                Row(modifier = Modifier.padding(top = Dimens.grid_2, bottom = Dimens.grid_2)) {
+                        @Suppress("MagicNumber")
+                        Column(
+                                modifier =
+                                        Modifier.padding(
+                                                start = Dimens.grid_2_5,
+                                                end = Dimens.grid_2_5
+                                        )
+                        ) {
+                                Row(verticalAlignment = Alignment.CenterVertically) {
+                                        ResponsiveText(
+                                                text = userPullRequestUIModel.pullRequest.title,
+                                                textStyle = typography.h3,
+                                                color = Colors.tertiary,
+                                                modifier =
+                                                        Modifier.padding(end = Dimens.grid_1)
+                                                                .weight(1f)
+                                        )
+                                        Text(
+                                                text =
+                                                        userPullRequestUIModel
+                                                                .formattedLastUpdatedTime
+                                                                .getString(),
+                                                fontSize = 10.sp,
+                                                style = typography.body1,
+                                                color = brown_grey
+                                        )
+                                }
+                                Row(
+                                        modifier =
+                                                Modifier.padding(top = Dimens.grid_0_5)
+                                                        .fillMaxWidth(),
+                                        verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                        Text(
+                                                text = userPullRequestUIModel.pullRequest.source,
+                                                style = typography.body1,
+                                                color = Colors.onBackground,
+                                                maxLines = 1,
+                                                modifier =
+                                                        Modifier.background(
+                                                                        color =
+                                                                                brown_grey.copy(
+                                                                                        alpha = 0.5f
+                                                                                ),
+                                                                        shape =
+                                                                                RoundedCornerShape(
+                                                                                        Dimens.grid_0_5
+                                                                                )
+                                                                )
+                                                                .padding(Dimens.grid_0_75)
+                                                                .weight(1f, fill = false)
+                                        )
+                                        Text(
+                                                text = "\u2192",
+                                                style = typography.body1,
+                                                modifier =
+                                                        Modifier.padding(horizontal = Dimens.grid_1)
+                                        )
+                                        Text(
+                                                text =
+                                                        userPullRequestUIModel
+                                                                .pullRequest
+                                                                .destination,
+                                                style = typography.body1,
+                                                color = Colors.onBackground,
+                                                maxLines = 1,
+                                                modifier =
+                                                        Modifier.background(
+                                                                        color =
+                                                                                brown_grey.copy(
+                                                                                        alpha = 0.5f
+                                                                                ),
+                                                                        shape =
+                                                                                RoundedCornerShape(
+                                                                                        Dimens.grid_0_5
+                                                                                )
+                                                                )
+                                                                .padding(Dimens.grid_0_75)
+                                                                .weight(1f, fill = false),
+                                                textAlign = TextAlign.End
+                                        )
+                                }
+                                Row(Modifier.fillMaxWidth().padding(top = Dimens.grid_1)) {
+                                        Text(
+                                                text = userPullRequestUIModel.pullRequest.author,
+                                                style =
+                                                        typography.body1.copy(
+                                                                fontWeight = FontWeight.Bold
+                                                        ),
+                                                color = greyish_brown
+                                        )
+                                        Text(
+                                                text = userPullRequestUIModel.pullRequest.reviewers,
+                                                style = typography.body1,
+                                                modifier = Modifier.weight(1f),
+                                                textAlign = TextAlign.End,
+                                                fontStyle = FontStyle.Italic,
+                                                color = greyish_brown
+                                        )
+                                }
+                        }
                 }
-                Row(
-                        modifier = Modifier.padding(top = Dimens.grid_0_5),
-                        horizontalArrangement = Arrangement.spacedBy(Dimens.grid_2)
-                ) {
-                    Text(
-                            text = userPullRequestUIModel.pullRequest.source,
-                            style = typography.body1,
-                            color = Colors.onBackground,
-                            modifier =
-                                    Modifier.background(
-                                                    color = brown_grey,
-                                                    shape = RoundedCornerShape(Dimens.grid_0_5)
-                                            )
-                                            .padding(Dimens.grid_0_75)
-                    )
-                    Text(text = "\u2192", style = typography.body1)
-                    Text(
-                            text = userPullRequestUIModel.pullRequest.destination,
-                            style = typography.body1,
-                            color = Colors.onBackground,
-                            modifier =
-                                    Modifier.background(
-                                                    color = brown_grey,
-                                                    shape = RoundedCornerShape(Dimens.grid_0_5)
-                                            )
-                                            .padding(Dimens.grid_0_75),
-                            textAlign = TextAlign.End
-                    )
-                }
-                Row(Modifier.fillMaxWidth().padding(top = Dimens.grid_1)) {
-                    Text(
-                            text = userPullRequestUIModel.pullRequest.author,
-                            style = typography.body1.copy(fontWeight = FontWeight.Bold)
-                    )
-                    Text(
-                            text = userPullRequestUIModel.pullRequest.reviewers,
-                            style = typography.body1,
-                            modifier = Modifier.weight(1f),
-                            textAlign = TextAlign.End,
-                            fontStyle = FontStyle.Italic
-                    )
-                }
-            }
         }
-    }
 }
 
 @Composable
 fun RepoDescriptionComponent(userRepositoryUiModel: UserRepositoryUiModel, modifier: Modifier) {
 
-    Column(modifier = modifier) {
-        Text(
-                userRepositoryUiModel.repo.name ?: "",
-                color = Colors.tertiary,
-                style = MaterialTheme.typography.h3.copy(fontWeight = FontWeight.Bold),
-                modifier = Modifier.padding(top = Dimens.grid_1).wrapContentHeight().fillMaxWidth()
-        )
-        Text(
-                userRepositoryUiModel.repo.owner?.displayName ?: "",
-                style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Normal),
-                modifier =
-                        Modifier.padding(top = Dimens.grid_0_5).wrapContentHeight().fillMaxWidth()
-        )
-        Text(
-                userRepositoryUiModel.repo.description ?: "",
-                style = MaterialTheme.typography.h6.copy(fontWeight = FontWeight.Light),
-                modifier =
-                        Modifier.padding(top = Dimens.grid_0_5, bottom = Dimens.grid_1)
-                                .wrapContentHeight()
-                                .fillMaxWidth()
-        )
-    }
+        Column(modifier = modifier) {
+                Text(
+                        userRepositoryUiModel.repo.name ?: "",
+                        color = Colors.tertiary,
+                        style = MaterialTheme.typography.h3.copy(fontWeight = FontWeight.Bold),
+                        modifier =
+                                Modifier.padding(top = Dimens.grid_1)
+                                        .wrapContentHeight()
+                                        .fillMaxWidth()
+                )
+                Text(
+                        userRepositoryUiModel.repo.owner?.displayName ?: "",
+                        style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Normal),
+                        modifier =
+                                Modifier.padding(top = Dimens.grid_0_5)
+                                        .wrapContentHeight()
+                                        .fillMaxWidth()
+                )
+                Text(
+                        userRepositoryUiModel.repo.description ?: "",
+                        style = MaterialTheme.typography.h6.copy(fontWeight = FontWeight.Light),
+                        modifier =
+                                Modifier.padding(top = Dimens.grid_0_5, bottom = Dimens.grid_1)
+                                        .wrapContentHeight()
+                                        .fillMaxWidth()
+                )
+        }
 }
 
 @PreviewComposable
 @Composable
 private fun HomeCardPreview() {
-    Preview { RepositoryCardLayout(userRepositoryUiModel = testCard1) {} }
+        Preview { RepositoryCardLayout(userRepositoryUiModel = testCard1) {} }
 }
